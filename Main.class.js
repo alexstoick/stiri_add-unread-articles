2d874dea9dfd88883f062074d27d17e7341090a7
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-08-24 12:10:48 +0300

    tweaking the main class

--- Main.class.js
+++ Main.class.js
@@ -51,29 +51,11 @@ Main.prototype.makeRequest = function ( url )
 Main.prototype.newArticle = function ( url , title , description , pubDate ) {
 
 	var self = this.current_instance ;
-	var newArticle = false ;
-
-	if ( self.currentDate < pubDate )
-	{
-		newArticle = true ;
-	}
 
 	self.redis.exists ( url , function ( err , res) {
 
 		if ( res == 0 )
 		{
-			if ( newArticle )
-			{
-				article = { url: url , title: title , description: null , date: pubDate } ;
-				self.articles.push ( article ) ;
-			}
-
-			self.articles_proccessed ++ ;
-			if ( self.articles_proccessed === self.count )
-			{
-				console.log ( 'finished') ;
-				self.emit ( 'finished' ) ;
-			}
 			//Setup key in redis
 			self.redis.set ( url , 'updated' ) ;
 
@@ -84,49 +66,7 @@ Main.prototype.newArticle = function ( url , title , description , pubDate ) {
 				self.addToSolrAndMySQL ( url , title , description , body , pubDate , self ) ;
 			}) ;
 		}
-		else
-		{
-
-			if ( newArticle )
-			{
-				self.mysql.getConnection ( function ( err , mysql_con) {
-					var connection = mysql_con ;
-					connection.query ( "SELECT text, created_at FROM articles WHERE url='" + url + "'" , function ( err , res) {
-
-						if ( res.length == 0 )
-						{
-							article = { url: url , title: title , description : null , date: pubDate } ;
-							self.articles.push ( article ) ;
-						}
-						else
-						{
-							article = { url: url , title: title ,  description : res[0].text , date: res[0].created_at } ;
-							self.articles.push ( article ) ;
-						}
-
-						self.articles_proccessed ++ ;
-						if ( self.articles_proccessed === self.count )
-						{
-							self.emit ( 'finished' ) ;
-						}
-						connection.end();
-					}) ;
-
-				});
-			}
-			else
-			{
-				self.articles_proccessed ++ ;
-				if ( self.articles_proccessed === self.count )
-				{
-					self.emit ( 'finished' ) ;
-				}
-			}
-
-		}
-
 	} ) ;
-
 }
 
 Main.prototype.addToSolrAndMySQL = function ( url , title , description , response , pubDate , instance )
@@ -151,6 +91,18 @@ Main.prototype.addToSolrAndMySQL = function ( url , title , description , respon
 
 		connection.query ( self.mysql_query , mysql_set , function ( err , res ) {
 			console.log ( 'Added to MySQL') ;
+			console.log ( res.insertId ) ;
+
+			article = { url: url , mysql_id: res.insertId } ;
+			self.articles.push ( article ) ;
+
+			self.articles_proccessed ++ ;
+			if ( self.articles_proccessed === self.count )
+			{
+				console.log ( 'finished') ;
+				self.emit ( 'finished' ) ;
+			}
+
 			if ( err )
 				console.log ( err ) ;
 			connection.end();
